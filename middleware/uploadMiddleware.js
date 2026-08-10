2429a9843efe99f0fb551c7d3518aeee7052ed6b
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2026-08-10 14:54:18 +0700

    add: implement uploadMiddleware to handle image uploads with multer

---
+++ middleware/uploadMiddleware.js
@@ -0,0 +1,39 @@
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+
+  filename: (req, file, cb) => {
+    const uniqueName = Date.now() + "-" + file.originalname;
+    cb(null, uniqueName);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = /jpeg|jpg|png/;
+
+  const extname = allowedTypes.test(
+    path.extname(file.originalname).toLowerCase(),
+  );
+
+  const mimetype = allowedTypes.test(file.mimetype);
+
+  if (extname && mimetype) {
+    cb(null, true);
+    } else {
+    cb(new Error("Only images are allowed (jpeg, jpg, png)"));
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+  limits: {
+    fileSize: 1 * 1024 * 1024
+    } // 1MB limit
+});
+
+module.exports = upload;
